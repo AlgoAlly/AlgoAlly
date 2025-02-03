13f0dbe5925e948def59af4610a23d3c34190aaf
Youssef A <Youssef.abouzied@mail.utoronto.ca>
2025-02-03 13:32:21 -0500

    feat(Profile-Page): Added UI for profile page, input fields + dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 const Navbar: React.FC = () => {
   return (
     <header className="sticky top-0 z-20 bg-[#191A23] text-white p-4 space-x-[161px] items-center justify-between border-b border-[#E0E1EC]/30">
-<nav className="flex space-x-[161px] items-center">
+<nav className="flex space-x-[161px] pl-30 items-center">
       <NavLink
         to="/problems"
-        className="text-primary text-[24px] hover:text-primary-hover hover:font-bold"
+        className="text-[#EEEFFC] text-[24px] hover:text-blue-500 hover:font-bold"
       >
         Problems
       </NavLink>
