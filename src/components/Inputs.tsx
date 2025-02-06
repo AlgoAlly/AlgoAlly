ca4e1d5b0a2016cfdee1f9163c22cbdb91812f89
Youssef A <Youssef.abouzied@mail.utoronto.ca>
2025-02-06 17:09:38 -0500

    feat(Profile-Page): bug fixes for PR

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
+// import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import Button from './Button';
 
 const Inputs = () => {
   const [firstName, setFirstName] = useState("");
@@ -33,6 +34,15 @@ const Inputs = () => {
     };
   }, []);
 
+  const submit = () => {
+    if (firstName === '' || lastName === '' || email === '' || username === '' || selectedCountry === "Select Country") {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    alert('submission successful!');
+  };
+
   return (
     <div>
     <div className="absolute top-54.5 left-80">
@@ -41,7 +51,7 @@ const Inputs = () => {
         value={firstName}
         onChange={(e) => setFirstName(e.target.value)}
         placeholder="First Name"
-        className="w-64 p-2 border border-[#393A4B] rounded-lg placeholder-[#EEEFFC] placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
+        className="w-64 p-2 border border-[#393A4B] rounded-lg placeholder-text-secondary placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
     </div>
     <div className="absolute top-54.5 left-150">
       <input
@@ -49,7 +59,7 @@ const Inputs = () => {
         value={lastName}
         onChange={(e) => setLastName(e.target.value)}
         placeholder="Last Name"
-        className="w-64 p-2 border border-[#393A4B] rounded-lg placeholder-[#EEEFFC] placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
+        className="w-64 p-2 border border-[#393A4B] rounded-lg placeholder-text-secondary placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
     </div>
     <div className="absolute top-72 left-80">
       <input
@@ -57,7 +67,7 @@ const Inputs = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Email"
-        className="w-134 p-2 border border-[#393A4B] rounded-lg placeholder-[#EEEFFC] placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
+        className="w-134 p-2 border border-[#393A4B] rounded-lg placeholder-text-secondary placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
     </div>
     <div className="absolute top-91.5 left-80">
       <input
@@ -65,24 +75,25 @@ const Inputs = () => {
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Username"
-        className="w-134 p-2 border border-[#393A4B] rounded-lg placeholder-[#EEEFFC] placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
+        className="w-134 p-2 border border-[#393A4B] rounded-lg placeholder-text-secondary placeholder:text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black]"      />
     </div>
     <div className="absolute top-108 left-80" ref={dropdownRef}>
-        <button
-          onClick={() => setIsOpen((prev) => !prev)}
-          className="w-70 p-2 border border-[#393A4B] text-left pl-3 rounded-lg placeholder-[#EEEFFC] text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] text-white caret-white focus:shadow-[0_0_0_2px_black] cursor-pointer flex justify-between items-center"
-        >
-          {selectedCountry}
-          <ChevronDownIcon
-            className={`w-5 h-5 text-white ml-2 transition-transform duration-200 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
-          />
+    <button 
+  onClick={() => setIsOpen((prev) => !prev)}
+  className="w-70 p-2 border border-[#393A4B] text-left pl-3 rounded-lg placeholder-text-secondary text-[15px] focus:placeholder:opacity-0 focus:outline-none bg-[#151621] focus:border-[#393A4B] caret-white focus:shadow-[0_0_0_2px_black] cursor-pointer flex justify-between items-center"
+>
+  <span className="text-secondary">{selectedCountry}</span>
+  {/* <ChevronDownIcon
+    className={`w-5 h-5 text-white ml-2 transition-transform duration-200 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+  /> */}
+
         </button>
         {isOpen && (
-          <ul className="absolute w-full mt-2 bg-[#151621] border border-[#393A4B] rounded-lg text-white">
+          <ul className="absolute w-full mt-2 bg-[#151621] border border-[#393A4B] rounded-lg text-secondary">
             {countries.map((country, index) => (
               <li
                 key={index}
-                className="p-2 hover:bg-gray-700 cursor-pointer"
+                className="p-2 hover:bg-gray-700 cursor-pointer text-secondary"
                 onClick={() => {
                   setSelectedCountry(country);
                   setIsOpen(false);
@@ -95,9 +106,13 @@ const Inputs = () => {
         )}
       </div>
       <div className="absolute top-130 left-20">
-        <button className="px-7 py-3.5 font-bold text-[20px] text-white rounded-lg hover:bg-indigo-700 border-4 border-zinc-600 bg-[#151621] cursor-pointer">
+      <Button
+          className="px-7 py-3.5 font-bold text-[20px] rounded-lg cursor-pointer" 
+          variant="primary"
+          onClick={() => submit()}
+        >
           Confirm
-        </button>
+        </Button>
       </div>
 </div>
     
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Navbar: React.FC = () => {
   return (
     <header className="sticky top-0 z-20 bg-[#191A23] text-white p-4 space-x-[161px] items-center justify-between border-b border-[#E0E1EC]/30">
-<nav className="flex space-x-[161px] pl-30 items-center">
+<nav className="flex space-x-[161px] items-center">
       <NavLink
         to="/problems"
         className="text-primary text-[24px] hover:text-primary-hover hover:font-bold"
@@ -23,11 +23,18 @@ const Navbar: React.FC = () => {
         About
       </NavLink>
       <NavLink
+        to="/login"
+        className="text-primary text-[24px] hover:text-primary-hover hover:font-bold"
+      >
+        Login
+      </NavLink>
+      <NavLink
         to="/home"
         className="text-primary text-[24px] hover:text-primary-hover hover:font-bold"
       >
         Home
       </NavLink>
+      
       </nav>
     </header>
   );
