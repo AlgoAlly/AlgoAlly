9e5e22b3db80320c8e5d3de318e4cc8e72354a7c
Youssef A <Youssef.abouzied@mail.utoronto.ca>
2025-02-06 17:09:38 -0500

    feat(Profile-Page): bug fixes for PR

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Navbar: React.FC = () => {
   return (
     <header className="sticky top-0 z-20 bg-[#191A23] text-white p-4 space-x-[161px] items-center justify-between border-b border-[#E0E1EC]/30">
-<nav className="flex space-x-[161px] pl-30 items-center">
+<nav className="flex space-x-[161px] items-center">
       <NavLink
         to="/problems"
         className="text-primary text-[24px] hover:text-primary-hover hover:font-bold"
