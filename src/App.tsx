c3bfcec9043032beb63477591b40a234859e3cce
Youssef A <Youssef.abouzied@mail.utoronto.ca>
2025-02-24 15:15:26 -0500

    merging

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Navbar/>
       <Routes>
         <Route path="/profile" element={<Profile />} />
         <Route path="/problems" element={<Problems />} />
