eef644177f886ece415a981fe56e8241ad612c61
Matthew Ramsoondar <ramsoondarmat@gmail.com>
2025-02-23 23:36:11 -0500

    add signup page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Profile from './pages/Profile';
 import Problems from './pages/Problems';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
