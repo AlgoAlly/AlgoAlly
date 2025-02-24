2654d70913dda630bdf3f62c000ece9beb0ba7a2
Matthew Ramsoondar <ramsoondarmat@gmail.com>
2025-02-23 23:36:11 -0500

    add signup page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,23 +1,36 @@
-import "react";
-import Navbar from '../components/Navbar'
-import Inputs from '../components/Inputs'
+import 'react';
+import Navbar from '../components/Navbar';
+import Inputs from '../components/Inputs';
 
 const Profile = () => {
-  return <div className='bg-[#191A23] min-h-screen'><Navbar></Navbar>
-  
+  return (
+    <div className="min-h-screen bg-[#191A23]">
+      <Navbar></Navbar>
 
-        <div>
-            <h1 className="text-[#D2D3E0] text-[36px] font-bold absolute top-25 left-20">Account</h1>
-            <h2  className="text-[#D2D3E0] text-[20px] font-medium absolute top-55 left-20">First & Last Name</h2>
-            <h3  className="text-[#D2D3E0] text-[20px] font-medium absolute top-73 left-20">Email</h3>
-            <h4  className="text-[#D2D3E0] text-[20px] font-medium absolute top-91 left-20">Username</h4>
-            <h5  className="text-[#858699] text-[12px] font-medium absolute top-97.5 left-20">You can change this later</h5>
-            <h6  className="text-[#D2D3E0] text-[20px] font-medium absolute top-109 left-20">Country</h6>
-        </div>
-        
-    
+      <div>
+        <h1 className="absolute top-25 left-20 text-[36px] font-bold text-[#D2D3E0]">
+          Account
+        </h1>
+        <h2 className="absolute top-55 left-20 text-[20px] font-medium text-[#D2D3E0]">
+          First & Last Name
+        </h2>
+        <h3 className="absolute top-73 left-20 text-[20px] font-medium text-[#D2D3E0]">
+          Email
+        </h3>
+        <h4 className="absolute top-91 left-20 text-[20px] font-medium text-[#D2D3E0]">
+          Username
+        </h4>
+        <h5 className="absolute top-97.5 left-20 text-[12px] font-medium text-[#858699]">
+          You can change this later
+        </h5>
+        <h6 className="absolute top-109 left-20 text-[20px] font-medium text-[#D2D3E0]">
+          Country
+        </h6>
+      </div>
 
-  <Inputs></Inputs></div>
+      <Inputs></Inputs>
+    </div>
+  );
 };
 
 export default Profile;
